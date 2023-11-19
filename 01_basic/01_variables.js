ac697a32dcb7e4519474645d42df0416846e6139
AdarshMundra <47634811+AdarshMundra@users.noreply.github.com>
2023-11-19 13:44:33 +0000

    01_Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =  12131351534
+let accountEmail = "Adarsh@yopmail.com"
+var accountPassword ="rfvb d5742f" 
+accountCity = "Balod"
+let accountState;
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// accountId = 2 // not allowed
+
+
+accountEmail = "am@am.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

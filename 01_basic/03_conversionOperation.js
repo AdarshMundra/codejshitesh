8f4ee7d3e8a44cd8feac212fa687122ef26f44a1
AdarshMundra <47634811+AdarshMundra@users.noreply.github.com>
2023-11-20 03:02:19 +0000

    conversionOperation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,72 @@
+let score =75;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let score1 ="";
+// let score1 =null;
+// let score1 =undefined;
+// let score1 ="75a";
+let score1 =true;
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+
+// console.log(typeof score1);
+// console.log(typeof(score1));
+
+let value1 = Number(score1)
+// console.log(typeof value1);
+// console.log(value1);
+
+let isLoggedIn = "adarsh"
+// 1 => true; 0 => false
+// "" => false
+// "adarsh" => true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Adarsh"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);

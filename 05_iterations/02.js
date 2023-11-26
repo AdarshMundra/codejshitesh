d338524497cdb1eae2aeda06de40a50fb787b485
AdarshMundra <47634811+AdarshMundra@users.noreply.github.com>
2023-11-26 05:56:01 +0000

    02

---
+++ 05_iterations/02.js
@@ -0,0 +1,19 @@
+let index = 0;
+while (index<10) {
+//     console.log(index);
+    index+=1;
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score =11;
+do {
+    console.log(`value of score${score}`);
+    score +=1
+} while (score<10);

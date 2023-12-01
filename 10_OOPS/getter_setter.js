4247aaef444b00d6471da355b08da7369c8b5516
AdarshMundra <47634811+AdarshMundra@users.noreply.github.com>
2023-12-01 15:08:21 +0000

    done

---
+++ 10_OOPS/getter_setter.js
@@ -0,0 +1,23 @@
+class user{
+    constructor(email,password){
+        this.email = email;
+        this.password = password;
+    }
+    get email(){
+        return this._email.toUpperCase();
+    }
+    set email(value){
+        this._email = value; 
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const Adarsh = new user("Adarsh@am.ai", "abc")
+console.log(Adarsh.email);
